04ee12c6969b248b086c768fe793f4124db538db
ale2cs <ale2@ualberta.ca>
2023-05-27 19:38:17 -0600

    Refactored with utilility file

---
+++ sportbooks/sports_interaction.py
@@ -0,0 +1,98 @@
+import cloudscraper
+import jmespath
+
+bet_type_dict = {
+    27:"moneyline",
+    28:"total",
+    180:"spread", 
+    704:"total", 
+    716:"spread", 
+    795:"spread", 
+    977:"spread", 
+}
+sport_urls = [
+    "https://www.sportsinteraction.com/hockey/nhl-betting-lines", 
+    "https://www.sportsinteraction.com/basketball/nba-betting-lines/", 
+    "https://www.sportsinteraction.com/baseball/mlb-betting-lines/",
+    "https://www.sportsinteraction.com/baseball/national-league-betting-lines/",
+]
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0",
+    "Accept": "text/html, application/xhtml+xml",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "x-momentum": "true",
+    "DNT": "1",
+    "Alt-Used": "www.sportsinteraction.com",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin"
+}
+
+def get_sports_interaction():
+    # Scrapes Sports Interaction and appends results into database
+    markets = []
+   
+    scraper = cloudscraper.create_scraper()
+
+    game_urls = []
+
+    # scrape game urls to access more markets
+    for url in sport_urls:
+        resp = scraper.get(url, headers=headers).json()
+        games = resp['props']['games']
+        for game in games:
+            gamePath = game['gamePath']
+            game_urls.append(f"https://www.sportsinteraction.com{gamePath}")
+
+    sportsbook = "Sports Interaction"
+    period = 0  # only full game including OT 
+    dec = 3     # rounding down payouts
+
+    # expressions
+    bet_type_keys = [f'`{key}`' for key in bet_type_dict]
+    bet_type_query = f"[{', '.join(bet_type_keys)}]"
+    des_bets = ("gameData."
+                f"betTypeGroups[?contains({bet_type_query}, betTypeId)]")
+    market_exp = "betTypes[0].events[*].runners[*]"
+
+    # scrape markets from each game page
+    for url in game_urls:
+        resp = scraper.get(url, headers=headers).json()
+        data = resp['props']
+
+        matchup = data['game']['fullName']
+        to_replace = [' (A)', ' (N)']
+        for word in to_replace:
+            matchup = matchup.replace(word, '')
+        away, home = matchup.split(' @')
+
+        date = data['game']['date']
+        date = date.replace('.000', '')
+
+        # searches
+        bets = jmespath.search(des_bets, data)
+        for bet in bets:
+            bet_type = bet_type_dict[bet['betTypeId']]
+            order = [0, 1] if bet_type == "total" else [1, 0]
+            market_data = jmespath.search(market_exp, bet)
+            for market in market_data:
+                market_id = market['eventId']
+                home_payout = market[order[0]]['currentPrice']
+                away_payout = market[order[1]]['currentPrice']
+                spov = market[order[0]]['handicap']
+                spun = market[order[1]]['handicap']
+                if bet_type == 'spread':
+                    if (spov[0] == '-'):
+                        spun = f"-{spun}"
+                    elif (spun[0] == '-'):
+                        spov = f"+{spov}"
+                markets.append((market_id, sportsbook, matchup, bet_type, 
+                    period, date, home, away, home_payout, away_payout, spun, 
+                    spov
+                ))
+    return markets
+
+def request():
+    
---
+++ utils.py
@@ -0,0 +1,55 @@
+import math
+import datetime
+
+def rnd_dec(number, decimals):
+    # Returns a value rounded down to a specific number of decimal places.
+    if not isinstance(decimals, int):
+        raise TypeError("decimal places must be an integer")
+    elif decimals < 0:
+        raise ValueError("decimal places has to be 0 or more")
+    elif decimals == 0:
+        return math.floor(number)
+
+    factor = 10 ** decimals
+    return math.floor(number * factor) / factor
+
+def american_to_decimal(number):
+    dec = 3
+    if (number > 0):
+        converted = rnd_dec(1 + (number / 100), dec)
+    else:
+        converted = rnd_dec(1 - (100 / number), dec) 
+
+    return converted
+
+def imp_prob(odds1, odds2):
+    return (odds2 / (odds1 + odds2)), (odds1 / (odds1 + odds2))
+
+def vig(odds1, odds2):
+    return (1 / odds1) + (1 / odds2) - 1
+
+def no_vig_odds(odds1, odds2): 
+    prob1, prob2 = imp_prob(odds1, odds2)
+    return (1 / prob1), (1 / prob2) 
+
+def ev(win_prob, odds):
+    return (win_prob * (odds - 1)) + win_prob - 1 
+
+def pos_ev(odds1, odds2, odds3, odds4):
+    prob1, prob2 = imp_prob(odds1, odds2)
+    ev1 = ev(prob1, odds3)
+    ev2 = ev(prob2, odds4)
+    return ev1 if (ev1 > 0) else ev2
+
+def kelly_criterion(win_prob, odds):
+    return win_prob - ((1 - win_prob) / (odds - 1))
+
+def wager(bankroll, odds1, odds2, odds3): 
+    prob1, prob2 = imp_prob(odds1, odds2)
+    multiplier = 0.3
+    kelly = kelly_criterion(prob1, odds3)
+    return multiplier * kelly * bankroll
+
+def rem_time(time):
+    return datetime.fromisoformat(time) - datetime.fromisoformat(datetime.now(timezone.utc).isoformat())
+
